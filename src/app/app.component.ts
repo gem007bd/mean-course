fce0e948264cbfd75a6c2614e7ec0d4d2e3441e6
gem007bd <gem007bd@yahoo.com>
2019-01-08 17:00:19 +0100

    add material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'mean-course';
+  stroedPosts = [];
+
+  onPostAdded(post) {
+    this.stroedPosts.push(post);
+  }
 }
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.less']
+})
+export class PostListComponent {
+  posts: Array<Object> = [];
+  // posts = [
+  //   { title: 'first post', content: 'this is the first post content' },
+  //   { title: 'seond post', content: 'this is the second post content' },
+  //   { title: 'third post', content: 'this is the third post content' },
+  // ];
+  @Input() posts;
+}
